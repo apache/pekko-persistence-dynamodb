73c28dcaa9abf5eaacd93388976e3ca24d929159
Joost de Vries <joost@ziener.org>
2017-07-23 09:22:08 +0200

    move to test

--- src/main/scala/akka/persistence/dynamodb/snapshot/package.scala
+++ src/main/scala/akka/persistence/dynamodb/snapshot/package.scala
@@ -21,23 +21,4 @@ package object snapshot {
   val Payload = "pay"
   val SequenceNr = "seq"
 
-  import com.amazonaws.services.dynamodbv2.model.{ KeySchemaElement, KeyType }
-
-  val schema = new CreateTableRequest()
-    .withAttributeDefinitions(
-      new AttributeDefinition().withAttributeName(Key).withAttributeType("S"),
-      new AttributeDefinition().withAttributeName(SequenceNr).withAttributeType("N"),
-      new AttributeDefinition().withAttributeName(Timestamp).withAttributeType("N")
-    )
-    .withKeySchema(
-      new KeySchemaElement().withAttributeName(Key).withKeyType(KeyType.HASH),
-      new KeySchemaElement().withAttributeName(SequenceNr).withKeyType(KeyType.RANGE)
-    )
-    .withLocalSecondaryIndexes(
-      new LocalSecondaryIndex()
-        .withIndexName(TimestampIndex).withKeySchema(
-          new KeySchemaElement().withAttributeName(Key).withKeyType(KeyType.HASH),
-          new KeySchemaElement().withAttributeName(Timestamp).withKeyType(KeyType.RANGE)
-        ).withProjection(new Projection().withProjectionType(ProjectionType.ALL))
-    )
 }
--- src/test/scala/akka/persistence/dynamodb/snapshot/DynamoDBUtils.scala
+++ src/test/scala/akka/persistence/dynamodb/snapshot/DynamoDBUtils.scala
@@ -31,6 +31,26 @@ trait DynamoDBUtils {
 
   implicit val timeout = Timeout(5.seconds)
 
+  import com.amazonaws.services.dynamodbv2.model.{ KeySchemaElement, KeyType }
+
+  val schema = new CreateTableRequest()
+    .withAttributeDefinitions(
+      new AttributeDefinition().withAttributeName(Key).withAttributeType("S"),
+      new AttributeDefinition().withAttributeName(SequenceNr).withAttributeType("N"),
+      new AttributeDefinition().withAttributeName(Timestamp).withAttributeType("N")
+    )
+    .withKeySchema(
+      new KeySchemaElement().withAttributeName(Key).withKeyType(KeyType.HASH),
+      new KeySchemaElement().withAttributeName(SequenceNr).withKeyType(KeyType.RANGE)
+    )
+    .withLocalSecondaryIndexes(
+      new LocalSecondaryIndex()
+        .withIndexName(TimestampIndex).withKeySchema(
+        new KeySchemaElement().withAttributeName(Key).withKeyType(KeyType.HASH),
+        new KeySchemaElement().withAttributeName(Timestamp).withKeyType(KeyType.RANGE)
+      ).withProjection(new Projection().withProjectionType(ProjectionType.ALL))
+    )
+
   def ensureSnapshotTableExists(read: Long = 10L, write: Long = 10L): Unit = {
     val create = schema
       .withTableName(SnapshotTable)
