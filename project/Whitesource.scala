dc4dc81cd7ff6154ba20821e4cdd6cc95eccd950
PJ Fanning <pjfanning@users.noreply.github.com>
2023-03-06 10:38:26 +0100

    add Apache headers (#20)
    * add Apache headers
    
    * main branch

--- project/Whitesource.scala
+++ project/Whitesource.scala
@@ -1,3 +1,12 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, derived from Akka.
+ */
+
 import sbt._
 import sbt.Keys._
 import sbtwhitesource.WhiteSourcePlugin.autoImport._
@@ -13,9 +22,9 @@ object Whitesource extends AutoPlugin {
     // do not change the value of whitesourceProduct
     whitesourceProduct := "Lightbend Reactive Platform",
     whitesourceAggregateProjectName := {
-      val projectName = (moduleName in LocalRootProject).value.replace("-root", "")
+      val projectName = (LocalRootProject / moduleName).value.replace("-root", "")
       projectName + "-" + (if (isSnapshot.value)
-                             if (gitCurrentBranch.value == "master") "master"
+                             if (gitCurrentBranch.value == "main") "main"
                              else "adhoc"
                            else
                              CrossVersion
--- src/test/scala/org/apache/pekko/persistence/dynamodb/journal/DynamoDBJournalSpec.scala
+++ src/test/scala/org/apache/pekko/persistence/dynamodb/journal/DynamoDBJournalSpec.scala
@@ -1,6 +1,16 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, derived from Akka.
+ */
+
 /**
  * Copyright (C) 2016 Typesafe Inc. <http://www.typesafe.com>
  */
+
 package org.apache.pekko.persistence.dynamodb.journal
 
 import org.apache.pekko.actor.ActorRef
